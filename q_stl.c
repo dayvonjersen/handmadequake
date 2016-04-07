4c276334f3043edc72925a859336048d17ea8a85
tso <tso@teknik.io>
2016-04-07 03:04:01 -0400

    Split into multiple files

---
+++ q_stl.c
@@ -0,0 +1,56 @@
+#include "quakedef.h"
+
+int32 Q_strcmp(uint8 *s1, uint8 *s2) {
+	while (*s1 == *s2) {
+		if (!*s1) return 0;
+		++s1;
+		++s2;
+	}
+	return (*s1 < *s2) ? -1 : 1;
+}
+
+int32 Q_atoi(uint8 *str) {
+	int32 sign = 1;
+	int32 val = 0;
+	uint8 c;
+
+	// negative
+	if (*str == '-') {
+		sign = -1;
+		str++;
+	}
+
+	// hexadecimal
+	if (*str == '0' && (str[1] == 'x' || str[2] == 'X')) {
+		str += 2;
+		while (1) {
+			c = *str;
+			str++;
+			if (c >= '0' && c <= '9') {
+				val = val * 16 + c - '0';
+			}
+			else if (c >= 'a' && c <= 'f') {
+				val = val * 16 + c - 'a' + 10;
+			}
+			else if (c >= 'A' && c <= 'F') {
+				val = val * 16 + c - 'A' + 10;
+			}
+			else {
+				return val * sign;
+			}
+		}
+	}
+	else {
+
+		// decimal
+		while (1) {
+			c = *str;
+			str++;
+			if (c < '0' || c > '9') {
+				return sign * val;
+			}
+			val = val * 10 + c - '0';
+		}
+	}
+	return sign * val;
+}
