1d5948af99bb75f94cec70163e207bce56af6e25
tso <tso@teknik.io>
2016-04-06 21:39:16 -0400

    1.2 parsing command line

---
+++ sys_win.c
@@ -0,0 +1,42 @@
+#include <Windows.h>
+
+#define MAX_NUM_ARGVS 50
+
+int argc = 0;
+char *largv[MAX_NUM_ARGVS];
+
+int COM_CheckParm(char *parm) {
+	for (int i = 0; i < argc; i++) {
+		if (!strcmp(parm, largv[i])) {
+			return i;
+		}
+	}
+	return 0;
+}
+
+int	CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	largv[0] = "";
+
+	while (*lpCmdLine && argc < MAX_NUM_ARGVS) {
+		while (*lpCmdLine && ( *lpCmdLine <= 32 || *lpCmdLine > 126 ) ) {
+			lpCmdLine++;
+		}
+		if (*lpCmdLine) {
+			largv[argc] = lpCmdLine;
+			argc++;
+			while (*lpCmdLine && *lpCmdLine > 32 && *lpCmdLine <= 126) {
+				lpCmdLine++;
+			}
+			if (*lpCmdLine) {
+				*lpCmdLine = 0;
+				lpCmdLine++;
+			}
+		}
+	}
+
+	largv[argc] = "";
+
+	int test = COM_CheckParm("-setalpha");
+		
+	return 0;
+}
