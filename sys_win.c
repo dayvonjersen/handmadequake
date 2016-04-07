689f2d08f2d9ad59f3b5e811ae774127892c2478
tso <tso@teknik.io>
2016-04-07 02:26:19 -0400

    1.3 replace strcmp and atoi

--- sys_win.c
+++ sys_win.c
@@ -20,6 +20,52 @@ int32 Q_strcmp(uint8 *s1, uint8 *s2) {
 	return (*s1 < *s2) ? -1 : 1;
 }
 
+int32 Q_atoi(uint8 *str) {
+	int32 sign = 1;
+	int32 val = 0;
+	uint8 c;
+
+	// negative
+	if (*str == '-') {
+		sign = -1;
+		str++;
+	}
+
+	// hexadecimal
+	if (*str == '0' && (str[1] == 'x' || str[2] == 'X')) {
+		str += 2;
+		while (1) {
+			c = *str;
+			str++;
+			if (c >= '0' && c <= '9') {
+				val = val * 16 + c - '0';
+			}
+			else if (c >= 'a' && c <= 'f') {
+				val = val * 16 + c - 'a' + 10;
+			}
+			else if (c >= 'A' && c <= 'F') {
+				val = val * 16 + c - 'A' + 10;
+			}
+			else {
+				return val * sign;
+			}
+		}
+	}
+	else {
+
+		// decimal
+		while (1) {
+			c = *str;
+			str++;
+			if (c < '0' || c > '9') {
+				return sign * val;
+			}
+			val = val * 10 + c - '0';
+		}
+	}
+	return sign * val;
+}
+
 int32 COM_CheckParm(uint8 *parm) {
 	for (int32 i = 0; i < argc; i++) {
 		if (!Q_strcmp(parm, largv[i])) {
@@ -50,7 +96,7 @@ int32 CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmd
 	}
 	
 	int32 test = COM_CheckParm("-setalpha");
-	int32 value = atoi(largv[test + 1]);
+	int32 value = Q_atoi(largv[test + 1]);
 		
 	return 0;
 }
