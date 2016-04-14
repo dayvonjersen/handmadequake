8171256df83c34ddda73b4a4ac3b1e228136aee7
tso <tso@teknik.io>
2016-04-14 10:42:02 -0400

    packfile listing (neat!)

--- tmp/4.3/source.c
+++ tmp/4.3/source.c
@@ -17,18 +17,37 @@ typedef struct {
     int32_t FileLength;
 } dpackfile_t;
 
+typedef struct {
+    char FileName[64];
+    int32_t FilePosition;
+    int32_t FileLength;
+} packfile_t;
+
+#define MAX_FILES_IN_PACK 2048
 int main(void) {
     int PakSize;
     int PakHandle = Sys_FileOpenRead("PAK0.PAK", &PakSize);
 
     dpackheader_t PakHeader;
     Sys_FileRead(PakHandle, &PakHeader, sizeof(PakHeader));
+    
+    int numFiles = PakHeader.DirectoryLength / sizeof(dpackfile_t);
+    packfile_t *FilesInMemory = malloc(numFiles * sizeof(packfile_t));
+
+    Sys_FileSeek(PakHandle, PakHeader.DirectoryOffset);
+
+    dpackfile_t tempPackFile[MAX_FILES_IN_PACK];
+    Sys_FileRead(PakHandle, (void *)tempPackFile, PakHeader.DirectoryLength);
 
     Sys_FileClose(PakHandle);
-    
+
     printf("PAK0.PAK: %d bytes\n", PakSize);
     printf("PakHeader.Magic: %c%c%c%c\n", PakHeader.Magic[0],PakHeader.Magic[1],PakHeader.Magic[2],PakHeader.Magic[3]);
     printf("PakHeader.DirectoryOffset: %d\n", PakHeader.DirectoryOffset);
     printf("PakHeader.DirectoryLength: %d\n", PakHeader.DirectoryLength);
-    printf("Number of files: %d\n", PakHeader.DirectoryLength / sizeof(dpackfile_t));
+    printf("Number of files: %d\n", numFiles);
+
+    for(int i = 0; i < numFiles; i++) {
+        printf("%04d: %- 56s [%d bytes]\n", i+1, tempPackFile[i].FileName, tempPackFile[i].FileLength);
+    }
 }
