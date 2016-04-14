1af080aace3ab2760e78e5c43c9e40b34245e2b3
tso <tso@teknik.io>
2016-04-14 10:30:27 -0400

    dpackfile_t and number of files in pak

--- tmp/4.3/source.c
+++ tmp/4.3/source.c
@@ -11,10 +11,24 @@ typedef struct {
     int32_t DirectoryLength;
 } dpackheader_t;
 
+typedef struct {
+    char FileName[56];
+    int32_t FilePosition;
+    int32_t FileLength;
+} dpackfile_t;
+
 int main(void) {
     int PakSize;
     int PakHandle = Sys_FileOpenRead("PAK0.PAK", &PakSize);
+
+    dpackheader_t PakHeader;
+    Sys_FileRead(PakHandle, &PakHeader, sizeof(PakHeader));
+
     Sys_FileClose(PakHandle);
     
     printf("PAK0.PAK: %d bytes\n", PakSize);
+    printf("PakHeader.Magic: %c%c%c%c\n", PakHeader.Magic[0],PakHeader.Magic[1],PakHeader.Magic[2],PakHeader.Magic[3]);
+    printf("PakHeader.DirectoryOffset: %d\n", PakHeader.DirectoryOffset);
+    printf("PakHeader.DirectoryLength: %d\n", PakHeader.DirectoryLength);
+    printf("Number of files: %d\n", PakHeader.DirectoryLength / sizeof(dpackfile_t));
 }
